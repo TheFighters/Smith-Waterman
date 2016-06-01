57a794c8405d4155f06bdfbcb0eb5f40c80dfc03
Hanoch Griner <eugriner@gmail.com>
2016-06-01 12:07:32 -0300

    REFATORANDO - Quebrado

---
+++ omp_smithW2.c
@@ -0,0 +1,404 @@
+/***********************************************************************
+ * Smith–Waterman algorithm
+ * Purpose:     Local alignment of nucleotide or protein sequences
+ * Authors:     Daniel Holanda, Hanoch Griner, Taynara Pinheiro
+ ***********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+/*--------------------------------------------------------------------
+ * Text Tweaks
+ */
+#define RESET   "\033[0m"
+#define BOLDRED "\033[1m\033[31m"      /* Bold Red */
+/* End of text tweaks */
+
+/*--------------------------------------------------------------------
+ * Constants
+ */
+#define PATH -1
+#define NONE 0
+#define UP 1
+#define LEFT 2
+#define DIAGONAL 3
+/* End of constants */
+
+/*--------------------------------------------------------------------
+* Helpers
+*/
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+#define max(a,b) ((a) > (b) ? a : b)
+
+#define DEBUG
+/* End of Helpers */
+
+
+/*--------------------------------------------------------------------
+ * Functions Prototypes
+ */
+void similarityScore(long long int i, long long int j, int* H, int* P, long long int* maxPos);
+int matchMissmatchScore(long long int i, long long int j);
+void backtrack(int* P, long long int maxPos);
+void printMatrix(int* matrix);
+void printPredecessorMatrix(int* matrix);
+void generate(void);
+long long int nElement(long long int i);
+void calcFirstDiagElement(long long int *i, long long int *si, long long int *sj);
+
+/* End of prototypes */
+
+
+/*--------------------------------------------------------------------
+ * Global Variables
+ */
+//Defines size of strings to be compared
+long long int m = 11; //Columns - Size of string a
+long long int n = 7;  //Lines - Size of string b
+
+//Defines scores
+int matchScore = 5;
+int missmatchScore = -3;
+int gapScore = -4;
+
+//Strings over the Alphabet Sigma
+char *a, *b;
+
+/* End of global variables */
+
+/*--------------------------------------------------------------------
+ * Function:    main
+ */
+int main(int argc, char* argv[]) {
+    int thread_count = strtol(argv[1], NULL, 10);
+    // m = strtoll(argv[2], NULL, 10);
+    // n = strtoll(argv[3], NULL, 10);
+
+#ifdef DEBUG
+    printf("\nMatrix[%lld][%lld]\n", n, m);
+#endif
+
+    //Allocates a and b
+    a = malloc(m * sizeof(char));
+    b = malloc(n * sizeof(char));
+
+    //Because now we have zeros
+    m++;
+    n++;
+
+    //Allocates similarity matrix H
+    int *H;
+    H = calloc(m * n, sizeof(int));
+
+    //Allocates predecessor matrix P
+    int *P;
+    P = calloc(m * n, sizeof(int));
+
+
+    //Gen rand arrays a and b
+    // generate();
+    a[0] =   'C';
+    a[1] =   'G';
+    a[2] =   'T';
+    a[3] =   'G';
+    a[4] =   'A';
+    a[5] =   'A';
+    a[6] =   'T';
+    a[7] =   'T';
+    a[8] =   'C';
+    a[9] =   'A';
+    a[10] =  'T';
+
+    b[0] =   'G';
+    b[1] =   'A';
+    b[2] =   'C';
+    b[3] =   'T';
+    b[4] =   'T';
+    b[5] =   'A';
+    b[6] =   'C';
+
+    
+    //Start position for backtrack
+    long long int maxPos = 0;
+    //Calculates the similarity matrix
+    long long int i, j;
+
+    //Gets Initial time
+    double initialTime = omp_get_wtime();
+
+    long long int si, sj;
+
+    //Because now we have zeros ((m-1) + (n-1) - 1)
+    long long int nDiag = m + n - 3;
+    long long int nEle;
+    
+    #pragma omp parallel num_threads(thread_count) default(none) private(i, j, si, sj, )
+    {
+        // printf("%lld %lld", i, j);
+        for (i = 1; i < nDiag; ++i)
+        {
+            nEle = nElement(i);
+            calcFirstDiagElement(&i, &si, &sj);
+            //printf("First Element of diagonal %lld is (%lld,%lld)\n",i, si,sj);
+            #pragma omp for
+            for (j = 1; j < nEle; ++j)
+            {
+                si -= j;
+                sj += j;
+                similarityScore(si, sj, H, P, &maxPos);
+                printf("si=%lld -\t sj=%lld \t i=%lld \t j=%lld\n",si,sj,i,j);
+            }
+        }
+    }
+
+    backtrack(P, maxPos);
+
+    //Gets final time
+    double finalTime = omp_get_wtime();
+    printf("\nElapsed time: %f\n", finalTime - initialTime);
+
+#ifdef DEBUG
+    printf("\nSimilarity Matrix:\n");
+    printMatrix(H);
+
+    printf("\nPredecessor Matrix:\n");
+    printPredecessorMatrix(P);
+#endif
+
+    //Frees similarity matrixes
+    free(H);
+    free(P);
+
+    //Frees input arrays
+    free(a);
+    free(b);
+
+    return 0;
+}  /* End of main */
+
+/*--------------------------------------------------------------------
+ * Function:    nElement
+ * Purpose:     Calculate the number of i-diagonal elements
+ */
+long long int nElement(long long int i) {
+    if (i < m && i < n) {
+        //Number of elements in the diagonal is increasing
+        return i + 1;
+    }
+    else if (i < max(m, n)) {
+        //Number of elements in the diagonal is stable
+        long int min = min(m, n);
+        return min;
+    }
+    else {
+        //Number of elements in the diagonal is decreasing
+        long int min = min(m, n);
+        return 2 * min - i + abs(m - n) - 1;
+    }
+}
+
+/*--------------------------------------------------------------------
+ * Function:    calcElement
+ * Purpose:     Calculate the position of (si, sj)-element
+ */
+void calcFirstDiagElement(long long int *i, long long int *si, long long int *sj) {
+    // Calculate the first element of diagonal
+    if (*i < n) {
+        *si = *i;
+        *sj = 0;
+    } else {
+        *si = n - 1;
+        *sj = *i - n + 1;
+    }
+}
+
+/*--------------------------------------------------------------------
+ * Function:    SimilarityScore
+ * Purpose:     Calculate  the maximum Similarity-Score H(i,j)
+ */
+void similarityScore(long long int i, long long int j, int* H, int* P, long long int* maxPos) {
+
+    int up, left, diag;
+
+    //Stores index of element
+    long long int index = m * i + j;
+
+    //Get element above
+    up = H[index - m] + gapScore;
+
+    //Get element on the left
+    left = H[index - 1] + gapScore;
+
+    //Get element on the diagonal
+    diag = H[index - m - 1] + matchMissmatchScore(i, j);
+
+    //Calculates the maximum
+    int max = NONE;
+    int pred = NONE;
+    /* === Matrix ===
+     *      a[0] ... a[n]
+     * b[0]
+     * ...
+     * b[n]
+     *
+     * generate 'a' from 'b', if '←' insert e '↑' remove
+     * a=GAATTCA
+     * b=GACTT-A
+     *
+     * generate 'b' from 'a', if '←' insert e '↑' remove
+     * b=GACTT-A
+     * a=GAATTCA
+    */
+
+    if (diag > max) { //same letter ↖
+        max = diag;
+        pred = DIAGONAL;
+    }
+
+    if (up > max) { //remove letter ↑
+        max = up;
+        pred = UP;
+    }
+
+    if (left > max) { //insert letter ←
+        max = left;
+        pred = LEFT;
+    }
+    //Inserts the value in the similarity and predecessor matrixes
+    H[index] = max;
+    P[index] = pred;
+
+    //Updates maximum score to be used as seed on backtrack
+    if (max > H[*maxPos]) {
+        #pragma omp critical
+        *maxPos = index;
+    }
+
+}  /* End of similarityScore */
+
+
+/*--------------------------------------------------------------------
+ * Function:    matchMissmatchScore
+ * Purpose:     Similarity function on the alphabet for match/missmatch
+ */
+int matchMissmatchScore(long long int i, long long int j) {
+    if (a[j - 1] == b[i - 1])
+        return matchScore;
+    else
+        return missmatchScore;
+}  /* End of matchMissmatchScore */
+
+/*--------------------------------------------------------------------
+ * Function:    backtrack
+ * Purpose:     Modify matrix to print, path change from value to PATH
+ */
+void backtrack(int* P, long long int maxPos) {
+    //hold maxPos value
+    long long int predPos;
+
+    //backtrack from maxPos to startPos = 0
+    do {
+        if (P[maxPos] == DIAGONAL)
+            predPos = maxPos - m - 1;
+        else if (P[maxPos] == UP)
+            predPos = maxPos - m;
+        else if (P[maxPos] == LEFT)
+            predPos = maxPos - 1;
+        P[maxPos] *= PATH;
+        maxPos = predPos;
+    } while (P[maxPos] != NONE);
+}  /* End of backtrack */
+
+/*--------------------------------------------------------------------
+ * Function:    printMatrix
+ * Purpose:     Print Matrix
+ */
+void printMatrix(int* matrix) {
+    long long int i, j;
+    for (i = 0; i < n; i++) { //Lines
+        for (j = 0; j < m; j++) {
+            printf("%d\t", matrix[m * i + j]);
+        }
+        printf("\n");
+    }
+
+}  /* End of printMatrix */
+
+/*--------------------------------------------------------------------
+ * Function:    printPredecessorMatrix
+ * Purpose:     Print predecessor matrix
+ */
+void printPredecessorMatrix(int* matrix) {
+    long long int i, j, index;
+    for (i = 0; i < n; i++) { //Lines
+        for (j = 0; j < m; j++) {
+            index = m * i + j;
+            if (matrix[index] < 0) {
+                printf(BOLDRED);
+                if (matrix[index] == -UP)
+                    printf("↑ ");
+                else if (matrix[index] == -LEFT)
+                    printf("← ");
+                else if (matrix[index] == -DIAGONAL)
+                    printf("↖ ");
+                else
+                    printf("- ");
+                printf(RESET);
+            } else {
+                if (matrix[index] == UP)
+                    printf("↑ ");
+                else if (matrix[index] == LEFT)
+                    printf("← ");
+                else if (matrix[index] == DIAGONAL)
+                    printf("↖ ");
+                else
+                    printf("- ");
+            }
+        }
+        printf("\n");
+    }
+
+}  /* End of printPredecessorMatrix */
+
+/*--------------------------------------------------------------------
+ * Function:    generate
+ * Purpose:     Generate arrays a and b
+ */
+void generate() {
+    //Generates the values of a
+    long long int i;
+    for (i = 0; i < m; i++) {
+        int aux = rand() % 4;
+        if (aux == 0)
+            a[i] = 'A';
+        else if (aux == 2)
+            a[i] = 'C';
+        else if (aux == 3)
+            a[i] = 'G';
+        else
+            a[i] = 'T';
+    }
+
+    //Generates the values of b
+    for (i = 0; i < n; i++) {
+        int aux = rand() % 4;
+        if (aux == 0)
+            b[i] = 'A';
+        else if (aux == 2)
+            b[i] = 'C';
+        else if (aux == 3)
+            b[i] = 'G';
+        else
+            b[i] = 'T';
+    }
+} /* End of generate */
+
+
+/*--------------------------------------------------------------------
+ * External References:
+ * http://vlab.amrita.edu/?sub=3&brch=274&sim=1433&cnt=1
+ * http://pt.slideshare.net/avrilcoghlan/the-smith-waterman-algorithm
+ * http://baba.sourceforge.net/
+ */
